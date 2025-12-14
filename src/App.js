7368d1d306779e44922f7675c24b97d8fabab37e
Student <student@d17-327e-pc-07.ipa.ii.agh.edu.pl>
2025-12-14 11:57:22 +0100

    List of elements

--- src/App.js
+++ src/App.js
@@ -5,15 +5,57 @@ import {useState} from "react";
 function App() {
     const [title, setTitle] = useState('Wall-E');
 
+    const movies = [
+
+        {title: "Wall-E"},
+
+        {title: "Pulp Fiction"},
+
+        {title: "Matrix"},
+
+        {title: "1670"},
+
+    ];
+
     function handleChange(event) {
         setTitle(event.target.value);
     }
 
+    let message;
+    if(title.length <= 0) {
+        message = 'Please enter a title';
+    }
+    else if(title.length < 5) {
+        message = 'Title must be at least 5 characters';
+    }
+    else if(title.length < 15) {
+        message = 'Title good';
+    }
+    else{
+        message = 'Title too long';
+    }
+
+    let myFavouriteMovie;
+    if(title.length > 0) {
+        myFavouriteMovie = "My favourite movie for today is " + title;
+    }
+    else{
+        myFavouriteMovie = null;
+    }
+
     return (
         <div>
-            <h1>My favourite movies to watch</h1>
-            <h2>My favourite movie for today is {title}</h2>
+            <h1>My favourite movies to watch test</h1>
+            <h2>{myFavouriteMovie}</h2>
+            <h2>{message}</h2>
             <input type='text' onChange={handleChange} />
+            <button type="button" onClick={() => alert(title)}>
+                Pokaż tytuł filmu
+            </button>
+
+            <ul>
+                {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+            </ul>
         </div>
     );
 }
