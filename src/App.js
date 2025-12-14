e88a31d1864c49a248951eaa09ec162a9c7f6356
Łukasz <luke.planeta@gmail.com>
2025-12-14 11:06:43 +0100

    update 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState('Wall-E');
+
+    function handleChange(event) {
+        setTitle(event.target.value);
+    }
+
+    return (
+        <div>
+            <h1>My favourite movies to watch</h1>
+            <h2>My favourite movie for today is {title}</h2>
+            <input type='text' onChange={handleChange} />
+        </div>
+    );
 }
 
 export default App;
